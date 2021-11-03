7459a1df434dea09299176af104dba7a754fa48e
smatiush <smatiushtechnology@gmail.com>
2021-11-03 09:02:40 +0100

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.gif';
 import './App.css';
 import detectEthereumProvider from '@metamask/detect-provider'
-var web3 = require('web3');
+import  { Redirect } from 'react-router-dom'
 
 class App extends Component {
 state = {
@@ -17,19 +17,29 @@ state = {
     const message = "welcome to Gambaru";
     const accounts = await ethereum.request({ method: 'eth_requestAccounts'});
     const account = accounts[0];
-    const signature = await ethereum.request({ method: 'personal_sign', params: [ message, account ]});
-    await this.callBackendAPI()
+    var signature = await ethereum.request({ method: 'personal_sign', params: [ message, account ]});
+    await this.callBackendAPI(signature)
   };
 
-  callBackendAPI = async () => {
-    const response = await fetch('/authenticated');
+  /*callBackendAPI = async (sig) => {
+    const response = await fetch('/admin')
     const body = await response;
-    console.log(body)
     if (response.status !== 200) {
       throw Error(body.message)
     }
+    console.log(body)
     return body;
-  };
+  };*/
+
+
+  callBackendAPI = async (sig) => {
+    const response = await fetch('/admin', {
+      method:'post',
+      body:JSON.stringify({
+        signature : sig})
+    })
+  }
+
 
   render() {
     return (
@@ -42,7 +52,7 @@ state = {
         <p className="App-intro">{this.state.data}</p>
       </div>
     );
-  }
-}
+  };
+};
 
 export default App;
