f59f45e237a9e393a1a61316fa7b56e48ccb3020
smatiush <smatiushtechnology@gmail.com>
2021-11-05 17:23:44 +0100

    add cross origin

--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const session = require('express-session');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,27 +6,28 @@ const router = express.Router();
 
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
-app.use(session({secret: 'ssshhhhh' ,saveUninitialized: false, resave: false}));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.post('/login',(req,res) => {
-    req.session.signature = req.body.signature;
-    res.end('done');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
+/*app.post('/login',(req,res) => {
+    req.session.signature = req.body.signature;
+    res.end('done');
+});*/
 
-router.post('/admin',(req,res) => {
+
+app.post('/admin',(req,res) => {
     console.log(req.body)
     if(req.body.signature) {
-        res.write(`<h1>Hello ${req.body.signature} h1><br>`);
+        //res.write(`<h1>Hello ${req.body.signature} h1><br>`);
+        res.redirect('/authenticated.html');
         //res.end('+'>Logout');
     } else {
         res.write('Please login first.');
         //res.end('+'>Login');
     }
-});
-
-app.get('/authenticated', function(req, res) {
-  res.redirect('/authenticated.html');;
 });
