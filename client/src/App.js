7d267589ca918c0f5db59190b8ad30dca0b7b29b
smatiush <smatiushtechnology@gmail.com>
2021-11-09 03:06:21 +0100

    bug fix, changed ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,64 @@
+
 import React, { Component } from 'react';
+import  { Redirect } from 'react-router-dom'
 import logo from './logo.gif';
 import './App.css';
 import detectEthereumProvider from '@metamask/detect-provider'
-import axios from 'axios'
-
-axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+import Session from "react-session-api"
+import Signature_badge from "./components/badge_view_sig.js"
+import axios from "axios"
+axios.defaults.baseURL = 'http://127.0.0.1:3001';
+axios.defaults.headers.post['Content-Type'] ='application/json';
 
 class App extends Component {
-state = {
-    data: null
-  };
+  constructor(){
+    super()
+    this.state = {
+            signature: ''
+        }
+    }
 
-  handleSignMessage = async () => {
+  Login = async () => {
     const ethereum = await detectEthereumProvider()
     const message = "welcome to Gambaru";
     const accounts = await ethereum.request({ method: 'eth_requestAccounts'});
     const account = accounts[0];
     var signature = await ethereum.request({ method: 'personal_sign', params: [ message, account ]});
-    await this.callBackendAPI(signature)
+    this.setState({signature: signature})
+    this.callBackendAPI()
+    return true
+  }
+
+  Handle_Login_Actvt_Comp = () => {
+    Session.set("signature", this.state.signature)
   };
 
 
-
-  callBackendAPI = async (sig) => {
-    axios.post('http://127.0.0.1:5000/admin',{
-      headers: {'Access-Control-Allow-Origin': 'http://127.0.0.1:5000/admin'},
-      params:sig
-    })
-    .then(() => console.log('signature sended'))
-    .catch(err => {
-        console.error(err);
-      });
-  }
-
+  callBackendAPI = () => {
+  var response = axios.post('http://127.0.0.1:3001/login', this.state)
+    .then((res)=>{return res})
+    Session.set("signature", response)
+}
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Gambaru  login with metamask</h1>
-          <button type="button" className='login_btn' onClick={this.handleSignMessage}>login</button>
+          <p>
+          <button type="button" className='login_btn' onClick={this.Login}>Login</button>
+          </p>
+          <button type="button" className='login_btn' onClick={this.Handle_Login_Actvt_Comp}>activate react component</button>
+          <div className="component-div set float-left">
+            <p className="badge badge-success badge-outlined">Signature from back_end:</p>
+            <Signature_badge />
+          </div>
+          <button type="button" className='login_btn' onClick={this.callBackendAPI}>call back-end:</button>
+          <div className="component-div set float-left">
+            <p className="badge badge-success badge-outlined">Signature from react session:</p>
+            <Signature_badge />
+          </div>
           </header>
-        <p className="App-intro">{this.state.data}</p>
       </div>
     );
   };
