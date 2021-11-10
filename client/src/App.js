cfe71daf169c2d42f657c53f8ddcdb6f0670eeb2
smatiush <smatiushtechnology@gmail.com>
2021-11-10 02:58:57 +0100

    added props mechanism to component
    modified html

--- client/src/App.js
+++ client/src/App.js
@@ -7,61 +7,61 @@ import detectEthereumProvider from '@metamask/detect-provider'
 import Session from "react-session-api"
 import Signature_badge from "./components/badge_view_sig.js"
 import axios from "axios"
-axios.defaults.baseURL = 'http://127.0.0.1:3001';
-axios.defaults.headers.post['Content-Type'] ='application/json';
+
 
 class App extends Component {
   constructor(){
+    axios.defaults.baseURL = 'http://127.0.0.1:3001';
+    axios.defaults.headers.post['Content-Type'] ='application/json';
+    axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*'
     super()
     this.state = {
-            signature: ''
+            signature: 'null'
         }
     }
 
-  Login = async () => {
+  login = async () => {
     const ethereum = await detectEthereumProvider()
     const message = "welcome to Gambaru";
     const accounts = await ethereum.request({ method: 'eth_requestAccounts'});
     const account = accounts[0];
-    var signature = await ethereum.request({ method: 'personal_sign', params: [ message, account ]});
-    this.setState({signature: signature})
-    this.callBackendAPI()
-    return true
+    let signature = await ethereum.request({ method: 'personal_sign', params: [ message, account ]});
+    this.state.signature = signature
+    console.log('session data:' + this.state.signature)
+    //await this.callBackendAPI()
+    //return true
   }
-
-  Handle_Login_Actvt_Comp = () => {
-    Session.set("signature", this.state.signature)
-  };
 
 
   callBackendAPI = () => {
-  var response = axios.post('http://127.0.0.1:3001/login', this.state)
-    .then((res)=>{return res})
-    Session.set("signature", response)
-}
+    this.login().then(()=>{let response = axios.post('http://127.0.0.1:3001/login', {'signature':this.state.signature})
+      .then((res)=>{return res})//promise pending
+      response.then((final_res) => {
+        if(final_res.data.logedIn == false){
+          console.log(final_res.data.logedIn)
+          return final_res.data.signature
+        }else{
+          console.log(final_res.data.signature)
+          return final_res.data.signature
+        }
+      })
+    });
+  }
+
 
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-          <button type="button" className='login_btn' onClick={this.Login}>Login</button>
-          </p>
-          <button type="button" className='login_btn' onClick={this.Handle_Login_Actvt_Comp}>activate react component</button>
-          <div className="component-div set float-left">
-            <p className="badge badge-success badge-outlined">Signature from back_end:</p>
-            <Signature_badge />
-          </div>
-          <button type="button" className='login_btn' onClick={this.callBackendAPI}>call back-end:</button>
-          <div className="component-div set float-left">
-            <p className="badge badge-success badge-outlined">Signature from react session:</p>
-            <Signature_badge />
-          </div>
-          </header>
-      </div>
-    );
-  };
-};
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <button type="button" className='login_btn' onClick={this.callBackendAPI}>call back-end</button>
+            <div className="component-div set float-left">
+                <p className="badge badge-success badge-outlined">Signature from back_end:</p>
+                <Signature_badge signature={this.callBackendAPI}/>
+            </div>
+            </header>
+        </div>);
+      };
+    };
 
 export default App;
